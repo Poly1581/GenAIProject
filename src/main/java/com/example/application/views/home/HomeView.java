057f0154756046660f4fb512639166b14ff33195
Brandon Paulsen <bspaulsen@dons.usfca.edu>
2024-10-16 19:55:04 -0700

    Added basic website structure

--- src/main/java/com/example/application/views/home/HomeView.java
+++ src/main/java/com/example/application/views/home/HomeView.java
@@ -3,6 +3,9 @@ package com.example.application.views.home;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.html.Paragraph;
@@ -14,6 +17,8 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 import ai.peoplecode.OpenAIConversation;
 
+import java.util.ArrayList;
+
 
 @PageTitle("Home")
 @Menu(icon = "line-awesome/svg/pencil-ruler-solid.svg", order = 0)
@@ -21,45 +26,114 @@ import ai.peoplecode.OpenAIConversation;
 @RouteAlias(value = "")
 public class HomeView extends Composite<VerticalLayout> {
 
-    private OpenAIConversation conversation;
-    private TextField askText;
-    private Paragraph replyText;
-    class MyClickListener
-            implements ComponentEventListener<ClickEvent<Button>> {
-        int count = 0;
+    private TextField userInput;
+    private City city1;
+    private City city2;
 
-        @Override
-        public void onComponentEvent(ClickEvent<Button> event) {
-            //event.getSource()
-            //        .setText("You have clicked me " + (++count) + " times");
-            String reply= conversation.askQuestion("You are Plato", askText.getValue());
-            replyText.setText(reply);
+    class City {
+        private String cityName;
+        private String context;
+        private Div responseContainer;
+        private final OpenAIConversation conversation = new OpenAIConversation("demo", "gpt-4o-mini");
+
+        public City(Div responseContainer, String cityName) {
+            this.responseContainer = responseContainer;
+            this.cityName = cityName;
+            this.context = "Use up to date information. You are arguing on in favor of visiting " + cityName;
+        }
+
+        public void askQuestion(String question) {
+            String answer = this.conversation.askQuestion(this.context, question);
+            Paragraph response = new Paragraph(answer);
+            this.responseContainer.add(response);
         }
     }
-    public HomeView() {
-        conversation = new OpenAIConversation("demo", "gpt-4o-mini");
-        askText = new TextField();
-        Button askButton = new Button();
-        replyText = new Paragraph();
-        replyText.setWidth("80%");
-        replyText.setHeight("300px");
-        replyText.getStyle().set("border", "1px solid black");
-        getContent().setWidth("100%");
-        getContent().getStyle().set("flex-grow", "1");
-        askText.setLabel("Ask Socrates a Question");
-        askText.setWidth("min-content");
-        askButton.setText("Ask");
-        askButton.setWidth("min-content");
-        askButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 
-        //replyText.setWidth("min-content");
-        getContent().add(askText);
-        getContent().add(askButton);
-        getContent().add(replyText);
+//    class MyClickListener
+//            implements ComponentEventListener<ClickEvent<Button>> {
+//        int count = 0;
+//
+//        @Override
+//        public void onComponentEvent(ClickEvent<Button> event) {
+//            //event.getSource()
+//            //        .setText("You have clicked me " + (++count) + " times");
+//            String reply= conversation.askQuestion("You are Plato", askText.getValue());
+//            replyText.setText(reply);
+//        }
+//    }
 
-        askButton.addClickListener(new MyClickListener());
-
+    private String getUserInput() {
+        String userMessage = userInput.getValue();
+        userInput.setValue("");
+        return userMessage;
     }
 
+    public HomeView() {
+        VerticalLayout appContainer = new VerticalLayout();
+        appContainer.getStyle().set("width", "100%");
+        appContainer.getStyle().set("height", "100%");
 
+        HorizontalLayout citiesContainer = new HorizontalLayout();
+        citiesContainer.getStyle().set("width", "100%");
+        citiesContainer.getStyle().set("height", "70%");
+        appContainer.add(citiesContainer);
+
+        VerticalLayout city1Container = new VerticalLayout();
+        city1Container.getStyle().set("width", "50%");
+        city1Container.getStyle().set("height", "100%");
+        citiesContainer.add(city1Container);
+
+        String city1ImagePath = "images/Hanoi.jpg";
+        Image city1Image = new Image(city1ImagePath, "");
+        city1Image.getStyle().set("width", "50%");
+        city1Container.add(city1Image);
+
+        Div city1Response = new Div();
+        city1Response.setHeight("80%");
+        city1Container.add(city1Response);
+
+        Paragraph city1Greeting = new Paragraph("Hello, I am Madrid.");
+        city1Greeting.getStyle().set("width", "100%");
+        city1Container.add(city1Greeting);
+
+        VerticalLayout city2Container = new VerticalLayout();
+        city2Container.getStyle().set("width", "50%");
+        city2Container.getStyle().set("height", "100%");
+        citiesContainer.add(city2Container);
+
+        String city2ImagePath = "images/Madrid.jpg";
+        Image city2Image = new Image(city2ImagePath, "");
+        city2Image.getStyle().set("width", "50%");
+        city2Container.add(city2Image);
+
+        Div city2Response = new Div();
+        city2Response.getStyle().set("height", "80%");
+        city2Container.add(city2Response);
+
+        Paragraph city2Greeting = new Paragraph("Hello, I am Madrid.");
+        city2Greeting.getStyle().set("width", "100%");
+        city2Container.add(city2Greeting);
+
+
+        HorizontalLayout buttonContainer = new HorizontalLayout();
+        buttonContainer.getStyle().set("width", "100%");
+        buttonContainer.getStyle().set("height", "10%");
+        appContainer.add(buttonContainer);
+
+        Button askCity1 = new Button("Ask city 1");
+        buttonContainer.add(askCity1);
+
+        Button askBothCities = new Button("Ask both cities");
+        buttonContainer.add(askBothCities);
+
+        Button askCity2 = new Button("Ask city 2");
+        buttonContainer.add(askCity2);
+
+        userInput = new TextField();
+        userInput.getStyle().set("height", "20%");
+        userInput.getStyle().set("width", "100%");
+        appContainer.add(userInput);
+        getContent().add(appContainer);
+
+    }
 }
